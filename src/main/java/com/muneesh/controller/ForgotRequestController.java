c567911dde61343911f63c1406b5105d8c390747
Muneesh <muneeshganisetti2@gmail.com>
2025-08-25 20:35:35 +0530

    changed the api view

--- src/main/java/com/muneesh/controller/ForgotRequestController.java
+++ src/main/java/com/muneesh/controller/ForgotRequestController.java
@@ -4,12 +4,13 @@ import com.muneesh.dto.ForgotPasswordRequest;
 import com.muneesh.service.ForgotPasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class ForgotRequestController {
     @Autowired
